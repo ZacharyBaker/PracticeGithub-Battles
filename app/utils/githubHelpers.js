c6b234238f94d5132f5f5faffee39f3b45b2e8fa
Zach Baker <zach.r.baker1@gmail.com>
2016-08-03 07:58:34 -0600

    working on api call

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo(username){
+	return axios.get('https://api.github.com/users/' + username + param)
+}
+
+
+var helpers = {
+	getPlayersInfo: function (players) {
+		//make the call
+	}
+}
+
+module.exports = helpers;
