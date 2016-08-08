3c8bfc7012bbde111abd35003656a06888f63991
Zach Baker <zach.r.baker1@gmail.com>
2016-08-08 07:51:04 -0600

    fixed bug

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -4,23 +4,22 @@ var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo(username){
-	return axios.get('https://api.github.com/users/' + username + param)
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username + param);
 }
-
 
 var helpers = {
-	getPlayersInfo: function (players) {
-		return axios.all(players.map(function(username){
-			return getUserInfo(username);
-		})).then(function (info){
-			return info.map(function (user){
-				return user.data
-			})
-		}).catch(function(err){
-			console.warn('Error in getPlayersInfo', err);
-		})
-	}
-}
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
+      })
+    })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  }
+};
 
 module.exports = helpers;
