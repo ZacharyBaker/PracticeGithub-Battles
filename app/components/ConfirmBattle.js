bc7aed08cf7b9c7e12e2e23075340619fe825643
Zach Baker <zach.r.baker1@gmail.com>
2016-08-11 07:49:38 -0600

    render func

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles')
 
 function puke (obj) {
   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
@@ -8,8 +9,28 @@ function puke (obj) {
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>LOADING</p>
-    : <div>CONFIRM BATTLE: {puke(props)}</div>
-}
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+        	<div className="col-sm-6">
+        		<p className="lead"> Player 1 </p>
+        		PLAYER 1 INFO
+        	</div>
+        	<div className="col-sm-6">
+        		<p className="lead"> Player 2 </p>
+        		PLAYER 2 INFO
+        	</div>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+        	<div className="col-sm-12" style={styles.space}>
+        		INITIATEBATTLEBUTTON
+        	</div>
+        	<div className="col-sm-12" style={styles.space}>
+        		Link to player 1
+        	</div>
+        </div>
+      </div>
+  }
 
 ConfirmBattle.propTypes = {
 	isLoading: PropTypes.bool.isRequired,
