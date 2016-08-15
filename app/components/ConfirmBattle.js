d965d657bc8f400278622e474bd8988b6bca8cb7
Zach Baker <zach.r.baker1@gmail.com>
2016-08-15 07:53:51 -0600

    prog

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles')
+var Link = require(react-router).Link;
 
 function puke (obj) {
   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
@@ -23,10 +24,14 @@ function ConfirmBattle (props) {
         </div>
         <div className="col-sm-8 col-sm-offset-2">
         	<div className="col-sm-12" style={styles.space}>
-        		INITIATEBATTLEBUTTON
+        		<button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+        		Initiate Battle
+        		</button>
         	</div>
         	<div className="col-sm-12" style={styles.space}>
-        		Link to player 1
+        		<Link to="/playerOne">
+        			<button type="button" className="btn btn-lg btn-danger">Reselect Players</button>
+        		</Link>
         	</div>
         </div>
       </div>
